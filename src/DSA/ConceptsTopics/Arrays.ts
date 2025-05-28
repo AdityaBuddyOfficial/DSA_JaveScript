fab50fc9c6080c41bb9743acbc35293487ad90fa
Aditya Gahile <aditya.gahile@vodafone.com>
2025-05-28 15:43:59 +0530

    all zeros at one side in array

--- src/DSA/ConceptsTopics/Arrays.ts
+++ src/DSA/ConceptsTopics/Arrays.ts
@@ -4,3 +4,5 @@
 // Array.push() this method is used to add element in Array
 
 // Array.pop() this method is used to remove element from the last of Array
+
+// so basically you are dealing with arr[i] in various form to figure out the solution
